d4d8c6088fea606603a2a3fe23ce1786e33148e7
Davatron <dwillisowen@icloud.com>
2025-02-03 20:55:38 +0000

    Creating blueprint to view issues

--- application/council/urls.py
+++ application/council/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import IssuesView, IssueDetailView
 
 urlpatterns = [
-    path("", IssuesView.as_view(), name="issues"),
+    path("", IssuesView.as_view(), name="home"),
     path("issue/<int:pk>", IssueDetailView.as_view(), name="issue-detail"),
 ]
--- application/council/views.py
+++ application/council/views.py
@@ -6,6 +6,8 @@ from .models import Issue
 class IssuesView(ListView):
     model = Issue
     template_name = "issues.html"
+    context_object_name = "issues"
+
 
 class IssueDetailView(DetailView):
     model = Issue
